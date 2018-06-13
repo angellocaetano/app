b1233966006a4d618dec219918d497a76e5fb6c7
angellocaetano <38465062+angellocaetano@users.noreply.github.com>
2018-06-13 10:37:16 -0300

    Initial commit

---
+++ routes/server.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+var app = express();
+
+app.get('/', function (req, res) {
+   
+    var sql = require("mssql");
+
+    // config for your database
+    var config = {
+        user: 'ncdb',
+        password: 'ncdb',
+        server: 'brsao00ssqlj102', 
+        database: 'ncdb' 
+    };
+
+    // connect to your database
+    sql.connect(config, function (err) {
+    
+        if (err) console.log(err);
+
+        // create Request object
+        var request = new sql.Request();
+           
+        // query to the database and get the records
+        request.query('select * from ncmast', function (err, recordset) {
+            
+            if (err) console.log(err)
+
+            // send records as a response
+            res.send(recordset);
+            
+        });
+    });
+});
+
+var server = app.listen(3000, function () {
+    console.log('Server is running..');
+    module.exports = router;
+});
